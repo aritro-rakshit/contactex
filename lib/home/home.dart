2f2cd24c89cb6b1e0b07a0679233f021553ab857
Aritro Rakshit <46522151+aritro-rakshit@users.noreply.github.com>
2022-08-15 09:44:42 +0530

    Initial commit

---
+++ lib/home/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(),
+      floatingActionButton:Container(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+    }
+}
